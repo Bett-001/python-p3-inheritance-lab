ec10e67174a056d4d1be46b3ac59572521c9941e
Tonny Bett <tonny.bett1@student.moringaschool.com>
2025-11-25 20:11:26 +0300

    Complete Inheritance lab

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,33 @@
 #!/usr/bin/env python
 
+# from user import User
+
+# import random
+
+# class Teacher(User):
+
+#     def teach(self):
+#         pass
+import random
 from user import User
 
-import random
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = knowledge
 
     def teach(self):
-        pass
+        # random index from 0 to len(self.knowledge) - 1
+        idx = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[idx]
